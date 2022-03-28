383bb34ec5166a8c3b0b7da52df108cc62bf6a3e
ZimmyCarrots <99004738+ZimmyCarrots@users.noreply.github.com>
2022-03-28 13:27:30 +0800

    answers for 2 question

--- day01-data-manipulation-and-functions/in-class/script.js
+++ day01-data-manipulation-and-functions/in-class/script.js
@@ -1,35 +1,40 @@
-var fahrenheitToCelsiusMain = function (input) {
+var fahrenheitToCelsiusMain = function (fahrenheit) {
   // Complete the Base: Fahrenheit to Celsius exercise below with fahrenheitToCelsiusMain as the main function.
-  var myOutputValue = 'hello world';
+  var convertToCelcius = ((fahrenheit - 32) * 5 / 9;
+  var myOutputValue = convertToCelcius;
   return myOutputValue;
 };
 
-var roadTripCostBaseMain = function (input) {
+var roadTripCostBaseMain = function (distanceInKm) {
   // Complete the Base: Road Trip Cost exercise below with roadTripCostBaseMain as the main function.
-  var myOutputValue = 'hello world';
+  // Convert Ferrari fuel usage in Km/L of fuel
+  var carfuelUsageInKm = 9 / 0.6213;
+  // If 1km (input) how many litter of fuel
+  var fuelCost = 2.2;
+  var myOutputValue = (distanceInKm / carfuelUsageInKm) * fuelCost;
   return myOutputValue;
 };
 
 var roadTripCostComfortableMain = function (input) {
   // Complete the Comfortable: Road Trip Cost exercise below with roadTripCostComfortableMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var iceMachineMain = function (input) {
   // Complete the More Comfortable: Ice Machine exercise below with iceMachineMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var beerOrderMain = function (input) {
   // Complete the More Comfortable: Beer Order exercise below with beerOrderMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var cellularDataMain = function (input) {
   // Complete the More Comfortable: Cost of Cellular Data exercise below with cellularDataMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
